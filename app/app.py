cf86316b6a6e4e7759044fd265c8741ae5e2d573
Sufiyan Bagwan <sufiyanbagwan17@gmail.com>
2026-09-03 17:20:28 +0000

    Test CI/CD pipeline

--- app/app.py
+++ app/app.py
@@ -6,7 +6,7 @@ app = Flask(__name__)
 @app.route("/")
 def home():
     return jsonify({
-        "message": "Hello from EKS cicd testing almsot done!",
+        "message": "Hello from EKS cicd testing v2!",
         "hostname": socket.gethostname(),
         "version": os.getenv("APP_VERSION", "v1")
     })
